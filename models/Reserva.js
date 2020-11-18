3f2c8aeb2e1c71dba765f2f3b4540b87f67612ad
Gabriel Cilico <gabrielcilico@gmail.com>
2020-11-18 18:46:37 -0300

    Adicionando model Reserva para controle de persistencia adas reservas

---
+++ models/Reserva.js
@@ -0,0 +1,72 @@
+const knex = require("../database/connection");
+const TABLE_NAME = 'reserva';
+
+class Reserva {
+    
+    async create(reserva) {
+        let {id_pessoa, id_laboratorio, data, hora} = reserva
+        try {
+            await knex.insert({id_pessoa, id_laboratorio, data, hora}).table(TABLE_NAME)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    async getById(id) {
+        try {
+            return await knex.select('*').table(TABLE_NAME).where({id: id})
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    async getAll() {
+        try {
+            return await knex.select('*').table(TABLE_NAME)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    async getByLaboratorio(id) {
+        try {
+            return await knex.select('*').table(TABLE_NAME).where({id_laboratorio: id})
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    async getByPessoa(id) {
+        try {
+            return await knex.select('*').table(TABLE_NAME).where({id_pessoa: id})
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    async getByData(data) {
+        try {
+            return await knex.select('*').table(TABLE_NAME).where({data: data})
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    async getByDataHora(data, hora) {
+        try {
+            return await knex.select('*').table(TABLE_NAME).where({data: data, hora: hora})
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    async cancela(id) {
+        try {
+            return await knex.delete().where({id: id}).table(TABLE_NAME)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
+module.exports = new Reserva()
