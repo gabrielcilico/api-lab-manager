a1261a7724d1338603cd4e5aa0e6460dce1c8a93
Gabriel Cilico <gabrielcilico@gmail.com>
2020-11-18 00:01:34 -0300

    Adicionando controle de Laboratorios e Pessoas

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,21 @@
 var express = require("express")
-var app = express();
 var router = express.Router();
-var HomeController = require("../controllers/HomeController");
+var PessoaController = require("../controllers/PessoaController");
+var LaboratorioController = require("../controllers/LaboratorioController");
 
-router.get('/', HomeController.index);
+
+// Pessoa
+router.post('/pessoa', PessoaController.create);
+router.get('/pessoa/:id', PessoaController.getById);
+router.get('/pessoas', PessoaController.getAll);
+
+// Laboratorio
+router.post('/laboratorio', LaboratorioController.create);
+router.get('/laboratorio/:id', LaboratorioController.getById);
+router.get('/laboratorios', LaboratorioController.getAll);
+router.get('/laboratorio/getByNome/:nome', LaboratorioController.getByNome);
+router.post('/laboratorio/getByData', LaboratorioController.getByData);
+router.post('/laboratorio/getByHora', LaboratorioController.getByHora);
+router.post('/laboratorio/getByDataHora', LaboratorioController.getByDataHora);
 
 module.exports = router;
