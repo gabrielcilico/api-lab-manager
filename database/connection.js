44f37a5c508695bff1f3ec534a2083387fefe87e
Gabriel Cilico <gabrielcilico@gmail.com>
2020-11-18 23:10:02 -0300

    Ajustando banco de dados

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
-    client: 'mysql2',
+    client: 'pg',
     connection: {
-      host : 'mysql741.umbler.com',
-      user : 'gabrielcilico',
-      password : 'abcdefg123456',
-      database : 'apilabmanager'
+      host : 'ec2-34-234-185-150.compute-1.amazonaws.com',
+      user : 'rnwkjjbbvdtuoe',
+      password : 'b5a06c2238574237d1a798630bcc2c5a9bf23c027621445bd1ad722f868b009b',
+      database : 'd3ufip6ubi6fnh'
     }
   });
 
