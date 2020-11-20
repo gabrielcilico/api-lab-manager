b19d20ac2f0d1eea0999d6284962d27fe68f6f90
Gabriel Cilico <gabrielcilico@gmail.com>
2020-11-19 22:30:28 -0300

    Add loggert

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -7,14 +7,16 @@ class HomeController {
 
     async criaLaboratorios(req,res) {
         for (let x = 100; (x-100) < 20; x++) {
-            await Laboratorio.create({
+            let obj = {
                 nome: `Laboratório ${x}`,
                 capacidade: (x-100)*3,
                 descricao: "Bloco 777",
                 foto: "https://cms.claretiano.edu.br/upload/claretianocms/33/pagina/192/foto/56.JPG",
                 dias_possiveis: "1,2,3,4,5,6",
                 horas_possiveis: "08:00,09:00,10:00,11:00,16:00,17:00,18:00,19:00,20:00,21:00,22:00"
-            })
+            }
+            await Laboratorio.create(obj)
+            console.log(obj)
         }
     }
 }
