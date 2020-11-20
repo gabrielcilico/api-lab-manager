36b8e4f07ada62717f1f56819e7ee090931b3beb
Gabriel Cilico <gabrielcilico@gmail.com>
2020-11-19 22:55:51 -0300

    Ajustando database

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : 'ec2-34-234-185-150.compute-1.amazonaws.com',
-      user : 'rnwkjjbbvdtuoe',
-      password : 'b5a06c2238574237d1a798630bcc2c5a9bf23c027621445bd1ad722f868b009b',
-      database : 'd3ufip6ubi6fnh'
+      host : '127.0.0.1',
+      user : 'root',
+      password : '',
+      database : 'apilabmanager'
     }
   });
 
